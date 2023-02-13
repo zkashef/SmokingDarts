e1dc01e60deec37e1a31521b3fd17311e1b4be0e
zkashef <zkashef@icloud.com>
2023-02-13 12:06:04 -0500

    area segmentation notebook created

--- scripts/open_heic_images.py
+++ scripts/open_heic_images.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 register_heif_opener()
 
-image = Image.open('/Users/mehdi/repos/dartboard-cv-score-system/data/heic/IMG_8540.HEIC')
+image = Image.open('/Users/zach/repos/dartboard-cv-score-system/data/heic/IMG_8540.HEIC')
 
 plt.imshow(image)
 plt.show()
